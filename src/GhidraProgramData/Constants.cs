400903f0c00741655a0734dbff3454be7bd3b671
Cam Sinclair <csinkers@gmail.com>
2025-11-03 20:32:34 +1000

    Fix warnings

--- src/GhidraProgramData/Constants.cs
+++ src/GhidraProgramData/Constants.cs
@@ -5,8 +5,17 @@ namespace GhidraProgramData;
 public static class Constants
 {
     public const uint PointerSize = 4;
+
+    /// <summary>
+    /// The namespace occupied by namespaces themselves and other reserved / internal types
+    /// </summary>
     public const string SpecialNamespace = "_";
+
+    /// <summary>
+    /// The name for the top-level namespace
+    /// </summary>
     public const string RootNamespaceName = "";
+
     public static Encoding Encoding { get; }
 
     static Constants()
